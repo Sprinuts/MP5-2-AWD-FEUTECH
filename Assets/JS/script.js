2d650884794392989464e2bce54fbdce1c058d40
Sprinuts <mlvgao18@gmail.com>
2024-03-09 09:31:50 +0800

    semi-final update

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -12,10 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     purchaseButton2.addEventListener('click', handlePurchase2);
     var purchaseButton3 = document.getElementById('purchaseButton3');
     purchaseButton3.addEventListener('click', handlePurchase3);
+    var purchaseButton4 = document.getElementById('purchaseButton3');
+    purchaseButton4.addEventListener('click', handlePurchase4);
 });
 
 function handlePurchase1() {
-    alert('Thank you for your purchase of Product 1!');
+    alert('Thank you for your purchase!');
     var modal = document.getElementById('product1-details-modal');
     var modalInstance = bootstrap.Modal.getInstance(modal);
     modalInstance.show();
@@ -23,14 +25,20 @@ function handlePurchase1() {
 
 function handlePurchase2() {
 
-    alert('Thank you for your purchase of Product 2!');
+    alert('Thank you for your purchase!');
     var modal = document.getElementById('product2-details-modal');
     var modalInstance = bootstrap.Modal.getInstance(modal);
     modalInstance.show();
 }
 function handlePurchase3() {
-    alert('Thank you for your purchase of Product 3!');
+    alert('Thank you for your purchase!');
     var modal = document.getElementById('product3-details-modal');
+    var modalInstance = bootstrap.Modal.getInstance(modal);
+    modalInstance.show();
+}
+function handlePurchase4() {
+    alert('Thank you for your purchase!');
+    var modal = document.getElementById('product4-details-modal');
     var modalInstance = bootstrap.Modal.getInstance(modal);
     modalInstance.show();
 }
